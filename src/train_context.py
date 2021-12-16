22f99e4bf79ea5b3b9d181330a1d5bc1a652f227
Chris Wang <christian.gnaw@gmail.com>
2021-12-16 11:50:36 -0500

    yeah

--- src/train_context.py
+++ src/train_context.py
@@ -22,64 +22,6 @@ PLT_INTERVAL = 10000
 
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# https://discuss.pytorch.org/t/how-to-add-noise-to-mnist-dataset-when-using-pytorch/59745
-# https://ai.plainenglish.io/denoising-autoencoder-in-pytorch-on-mnist-dataset-a76b8824e57e
-
-
-class AddGaussianNoise(object):
-    def __init__(self, mean=0., std=1., noise_factor=0.4):
-        self.std = std
-        self.mean = mean
-        self.noise_factor = noise_factor
-
-    def __call__(self, tensor):
-        noisy = tensor + self.noise_factor*torch.randn(tensor.size()) * self.std + self.mean
-        noisy = torch.clip(noisy, 0., 1.)
-        return noisy
-
-    def __repr__(self):
-        return self.__class__.__name__ + '(mean={0}, std={1})'.format(self.mean, self.std)
-
-
-class FrameDropout(object):
-    def __init__(self, num_frame_drop=5):
-        self.num_frame_drop = num_frame_drop
-
-    def dropout(self, tensor):
-        idx = np.random.randint(0, 10/NUM_FRAMES)
-        dropout = tensor[0:idx]
-        noise = torch.randn(tensor[idx].shape).unsqueeze(0)
-        dropout = torch.cat((dropout, noise, tensor[idx+1:]))
-        return dropout
-
-    def __call__(self, tensor):
-        # print(tensor.shape)
-        if self.num_frame_drop == 0 or tensor.shape[0] <= 1:
-            return tensor
-
-        dropout = self.dropout(tensor)
-        for i in range(0, self.num_frame_drop):
-            dropout = self.dropout(dropout)
-        '''
-        # generate dropout indices
-        dropout_indices = []
-        for i in range(self.num_frame_drop):
-            num = np.random.randint(0, 10/NUM_FRAMES)
-            while not num in dropout_indices:
-                num = np.random.randint(0, 10/NUM_FRAMES)
-            dropout_indices.append(num)
-        '''
-        return dropout
-
-
-noisy_transform = transforms.Compose([
-    # transforms.ToTensor(),
-    # transforms.Resize(32),
-    transforms.Normalize((0.1307,), (0.3081,)),
-    AddGaussianNoise(0., 1.),
-    FrameDropout(5)
-])
-
 
 original_data = MovingMNISTDataset(num_frames=NUM_FRAMES, transform=default_image_transform)
 noisy_data = MovingMNISTDataset(num_frames=NUM_FRAMES, transform=noisy_transform)
